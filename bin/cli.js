8b26811764e72e8338614fc18bec04d70e8f069c
BenSimmers <bej1380@gmail.com>
2022-09-04 15:45:27 +1000

    change cli file

--- bin/cli.js
+++ bin/cli.js
@@ -1,44 +1,37 @@
-#!/usr/bin/env node
-
-import { execSync } from 'child_process';
 
 
-const runCommand = (command) => {
+// #!/usr/bin/env node
+
+
+
+const { execSync } = require('child_process');
+
+const runCommand = command => {
   try {
-    execSync.execSync(command, { stdio: 'inherit' });
+    execSync(`${command}`, { stdio: 'inherit' });
+  } catch (e) {
+    console.error('Failed to execute ${command}', e);
+    return false;
   }
-  catch (error) {
-    console.error(`Error Found: ${error}`);
-    return 1;
-  }
-  return 0;
-}
-
-
-
+  return true;
+};
 
 const repoName = process.argv[2];
-
-const gitCheckoutCommand = `git clone https://github.com/BenSimmers/TypeScript-Vite-React-app.git ${repoName}`;
-
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/BenSimmers/TypeScript-Vite-React-app.git ${repoName}`;
 const installDepsCommand = `cd ${repoName} && npm install`;
 
-console.log("Cloning repo...  This may take a while...");
-
+console.log(`Cloning the repository with name ${repoName}`);
 const checkedOut = runCommand(gitCheckoutCommand);
+if (!checkedOut) process.exit(-1);
 
+console.log(`Installing dependencies for ${repoName}`);
+const installedDeps = runCommand(installDepsCommand);
+if (!installedDeps) process.exit(-1);
 
-if(!checkedOut) process.exit(code, -1);
-
-console.log(`Installing dependencies... for ${repoName}"`);
-const installedDepsCommand = runCommand(installDepsCommand);
-
-if(!installedDepsCommand){
-  process.exit(code, -1);
-}
-
-console.log(`${repoName} is ready to go!`);
-console.log(`To start the app, run: cd ${repoName} && npm start`);
+console.log(
+  'Congratulations! You are ready. Follow the following commands to start'
+);
+console.log(`cd ${repoName} && npm start`);
 
 
 
