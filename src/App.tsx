d836a0530c48163bf8d50d951e680803170d116a
Benjamin Simmers <bej1380@gmail.com>
2023-04-16 19:54:42 +1000

    new stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { ReactElement, useState } from 'react'
+import logo from './logo.svg'
+import viteLogo from './vite.svg'
+import tailwindLogo from './tailwind.svg'
 
-function App() {
+function App(): ReactElement {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div className="p-20 border shadow-xl border-gray-50 rounded-xl">
+      <header>
+        <div className="flex justify-center">
+          <img src={viteLogo} className="w-32 h-32" alt="vite logo" />
+          <img src={logo} className="w-32 h-32" alt="React logo" />
+          <img
+            src={tailwindLogo}
+            className="w-32 h-32"
+            alt="Tailwind CSS logo"
+          />
+        </div>
+        <p className="pb-3 text-2xl">Hello Vite + React + Tailwind CSS!</p>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          <button
+            className="pt-1 pb-1 pl-2 pr-2 text-sm text-purple-100 bg-purple-400 rounded"
+            onClick={() => setCount((count) => count + 1)}
+          >
+            count is: {count}
+          </button>
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <p className="pt-3 pb-3">
+          Edit{' '}
+          <code className="border border-1 pl-1 pr-1 pb-0.5 pt-0.5 rounded border-purple-400 font-mono text-sm bg-purple-100 text-purple-900">
+            src/App.tsx
+          </code>{' '}
+          and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="text-purple-400 underline"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="text-purple-400 underline"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+          {' | '}
+          <a
+            className="text-purple-400 underline"
+            href="https://tailwindcss.com/docs"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Tailwind CSS Docs
+          </a>
+        </p>
+      </header>
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
+import './App.css'
 import App from './App'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 )
