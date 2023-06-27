b5c84ffd7ce384cbd0a3c1ca467850e3aec25d16
Benjamin Simmers <bej1380@gmail.com>
2023-06-28 00:26:20 +1000

    feat: redo minified version and fix bugs in tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,45 @@
-import React, { ReactElement, useState } from 'react'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
-function App(): ReactElement {
-  const [count, setCount] = useState(0)
+function App() {
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="p-20 border shadow-xl border-gray-50 rounded-xl">
-      Hello world from React and Vite!
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick={() => setCount((count) => count + 1)}
-      >
-        count is: {count}
-      </button>
+    <div className="bg-gray-100 text-center min-h-screen flex flex-col items-center justify-center">
+      {/* put the two images next to eachother in a row */}
+
+      <div className="bg-white rounded-lg shadow-lg p-10 max-w-sm w-full space-y-2">
+        <div className="flex flex-row justify-center">
+          <img
+            src={reactLogo}
+            className="animate-spin-slow"
+            alt="logo"
+            width="200"
+            height="200"
+          />
+          <img
+            src={viteLogo}
+            className="animate-spin-slow"
+            alt="logo"
+            width="200"
+            height="200"
+          />
+        </div>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => setCount((count) => count + 1)}
+          >
+            count is: {count}
+          </button>
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
