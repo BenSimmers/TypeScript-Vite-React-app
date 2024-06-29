ee1034f50be0381154612dd3e7d3c78f649143ee
Benjamin Simmers <bej1380@gmail.com>
2024-06-29 14:59:49 +1000

    feat: add zustand for state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import { useDemoStore } from "./store/demoStore";
+import { useShallow } from "zustand/react/shallow";
 
 function App() {
-  const [count, setCount] = useState(0);
+
+  const { count } = useDemoStore(
+    useShallow((state) => ({ count: state.count }))
+  );
 
   return (
     <div className="bg-gray-100 text-center min-h-screen flex flex-col items-center justify-center">
-      {/* put the two images next to eachother in a row */}
-
       <div className="bg-white rounded-lg shadow-lg p-10 max-w-sm w-full space-y-2">
         <div className="flex flex-row justify-center">
           <img
@@ -32,7 +35,7 @@ function App() {
         <p>
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            onClick={() => setCount((count) => count + 1)}
+            onClick={() => useDemoStore.getState().increment()}
           >
             count is: {count}
           </button>
